138ce6b7c511ef7450300715f349bbdf2225d4c8
RoFearHypno <fearhypno@gmail.com>
2026-06-14 20:44:01 -0400

    Create SurfaceIds.lua

---
+++ Studio/Ids/SurfaceIds.lua
@@ -0,0 +1,26 @@
+return {
+	IdToSurface = {
+		[1]="Glue",
+		[2]="Hinge",
+		[3]="Inlet",
+		[4]="Motor",
+		[5]="Smooth",
+		[6]="SmoothNoOutlines",
+		[7]="SteppingMotor",
+		[8]="Studs",
+		[9]="Universal",
+		[10]="Weld"
+	},
+	SurfaceToId = {
+		["Glue"]=1,
+		["Hinge"]=2,
+		["Inlet"]=3,
+		["Motor"]=4,
+		["Smooth"]=5,
+		["SmoothNoOutlines"]=6,
+		["SteppingMotor"]=7,
+		["Studs"]=8,
+		["Universal"]=9,
+		["Weld"]=10
+	}
+}
