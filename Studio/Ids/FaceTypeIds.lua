d63d4325582319db77a172665ca5cf0edbe2b4e7
RoFearHypno <fearhypno@gmail.com>
2026-06-14 20:43:27 -0400

    Update FaceTypeIds.lua

--- Studio/Ids/FaceTypeIds.lua
+++ Studio/Ids/FaceTypeIds.lua
@@ -1,1 +1,18 @@
-
+return {
+	IdToFaceType={
+		[1]="Front",
+		[2]="Back",
+		[3]="Right",
+		[4]="Left",
+		[5]="Top",
+		[6]="Bottom"
+	},
+	FaceTypeToId={
+		["Front"]=1,
+		["Back"]=2,
+		["Right"]=3,
+		["Left"]=4,
+		["Top"]=5,
+		["Bottom"]=6
+	}
+}
